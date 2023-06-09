da825896d538f663d19cbd2354a273dc1a90e0cb
samyansan <41437406+samyansan@users.noreply.github.com>
2023-06-09 21:26:50 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -4,7 +4,7 @@
 curl -o i-1.txt https://raw.githubusercontent.com/samyansan/AdRules-SamYan/main/Rules/allowdomain.txt
 curl -o i-2.txt https://raw.githubusercontent.com/privacy-protection-tools/dead-horse/master/anti-ad-white-list.txt
 curl -o i-3.txt https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/optional-list.txt
-curl -o i-5.txt https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt
+curl -o i-4.txt https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
